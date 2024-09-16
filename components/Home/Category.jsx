1cba850f70a329899f2ad661bfb7590969c67c95
Prince Raj <praj.nitskm@gmail.com>
2024-09-16 13:01:19 +0530

    PetListByCategory added

--- components/Home/Category.jsx
+++ components/Home/Category.jsx
@@ -11,7 +11,7 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../config/FirebaseConfig";
 import Colors from "../../constants/Colors";
 
-export default function Category() {
+export default function Category({category}) {
   const [categoryList, setCategoryList] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("Cats");
   useEffect(() => {
@@ -46,7 +46,10 @@ export default function Category() {
         numColumns={4}
         renderItem={({ item, index }) => (
           <TouchableOpacity
-            onPress={() => setSelectedCategory(item.name)}
+            onPress={() => {
+              setSelectedCategory(item.name);
+              category(item.name);
+            }}
             style={{ flex: 1 }}
           >
             <View
--- components/Home/PetListByCategory.jsx
+++ components/Home/PetListByCategory.jsx
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Category from './Category'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { db } from '../../config/FirebaseConfig'
+import PetListItem from './PetListItem'
 
 export default function PetListByCategory() {
-  return (
+  
+    const [petList,setPetList] = useState([]);
+    const [loader,setLoader] = useState(false)
+
+    useEffect(() => {
+        GetPetList('Cats');
+    },[])
+
+    /**
+     * Used to get Pet List on Category Selection
+     * @param {*} category 
+     */
+
+    const GetPetList = async (category) => {
+        setLoader(true)
+        setPetList([])
+        const q = query(collection(db,'Pets'),where('category','==',category))
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach(doc => {
+            setPetList((prevPetList) => [...prevPetList,doc.data()])
+        })
+        setLoader(false)
+    }   
+
+    return (
     <View>
-        <Category />
+        <Category category={(value) => GetPetList(value)} />
+        <FlatList 
+        data={petList}
+        style={{ marginTop: 10 }}
+        refreshing={loader}
+        onRefresh={() => GetPetList('Cats')}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item,index}) => (
+            <PetListItem pet={item} />
+        )}
+        />
     </View>
   )
 }
