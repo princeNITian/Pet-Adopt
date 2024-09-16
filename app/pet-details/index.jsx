90b45039dd4b976606635b5d04df872eb5f46280
Prince Raj <praj.nitskm@gmail.com>
2024-09-16 22:12:26 +0530

    Adopt Me Button Added

--- app/pet-details/index.jsx
+++ app/pet-details/index.jsx
@@ -1,24 +1,31 @@
-import { View, Text, ScrollView } from 'react-native'
-import React, { useEffect } from 'react'
-import { useLocalSearchParams, useNavigation } from 'expo-router'
-import PetInfo from '../../components/PetDetails/PetInfo';
-import PetSubInfo from '../../components/PetDetails/PetSubInfo';
-import AboutPet from '../../components/PetDetails/AboutPet';
-import OwnerInfo from '../../components/PetDetails/OwnerInfo';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect } from "react";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import PetInfo from "../../components/PetDetails/PetInfo";
+import PetSubInfo from "../../components/PetDetails/PetSubInfo";
+import AboutPet from "../../components/PetDetails/AboutPet";
+import OwnerInfo from "../../components/PetDetails/OwnerInfo";
+import Colors from "../../constants/Colors";
 
 export default function PetDetails() {
-    const pet = useLocalSearchParams();
-    const navigation = useNavigation();
+  const pet = useLocalSearchParams();
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        navigation.setOptions({
-            headerTransparent: true,
-            headerTitle: ''
-        })
-    },[])
+  useEffect(() => {
+    navigation.setOptions({
+      headerTransparent: true,
+      headerTitle: "",
+    });
+  }, []);
   return (
     <View>
-        <ScrollView>
+      <ScrollView>
         {/* Pet Info */}
         <PetInfo pet={pet} />
 
@@ -30,8 +37,33 @@ export default function PetDetails() {
         {/* owner deatails */}
         <OwnerInfo pet={pet} />
         <View style={{ height: 70 }}></View>
-        </ScrollView>
-        {/* Adopt me button */}
+      </ScrollView>
+      {/* Adopt me button */}
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.adoptBtn}>
+          <Text
+            style={{
+              textAlign: "center",
+              fontFamily: "outfit-medium",
+              fontSize: 20,
+            }}
+          >
+            Adopt Me
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  adoptBtn: {
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+  },
+  bottomContainer: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0
+  },
+});
